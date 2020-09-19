f25217250c77bb70c1f95e80a714fd839cee6e31
Max Lockwood <59417148+Max88-git@users.noreply.github.com>
2020-09-19 10:52:35 +0100

    Initial commit

---
+++ question.js
@@ -0,0 +1,72 @@
+var questions = [{
+    "question": "Modern mountain bikes have what type of brakes?",
+    "option1": "Rim brakes",
+    "option2": "Disc brakes",
+    "option3": "Emergency brakes",
+    "option4": "Drum brakes",
+    "answer": "2"
+}, {
+    "question": "What is the name of the common device that changes your gears on a mountain bike?",
+    "option1": "Changer",
+    "option2": "Derailleur",
+    "option3": "Shifter",
+    "option4": "Gear Thing",
+    "answer": "2"
+}, {
+    "question": "Which one of these is NOT a brand of Mountain Biking apparel?",
+    "option1": "Fox",
+    "option2": "Troy Lee Designs",
+    "option3": "Dakine",
+    "option4": "Quiksilver",
+    "answer": "4"
+}, {
+    "question": "Which one of these is NOT a Mountain Bike brand?",
+    "option1": "Kona",
+    "option2": "All three are an MTB brand",
+    "option3": "Commencal",
+    "option4": "Intense",
+    "answer": "2"
+}, {
+    "question": "What is the world's largest Mountain Bike component brand?",
+    "option1": "SRAM",
+    "option2": "Avid",
+    "option3": "Hayes",
+    "option4": "Shimano",
+    "answer": "4"
+}, {
+    "question": "What term do Mountain Bikers use when in the lowest gear on a bike?",
+    "option1": "Granny Gear",
+    "option2": "1st Gear",
+    "option3": "Girly Gear",
+    "option4": "Easy Gear",
+    "answer": "1"
+}, {
+    "question": "What are the names of the metal wires that run from the hub to the rim of the wheel?",
+    "option1": "Rear Brakes",
+    "option2": "Forks",
+    "option3": "Stems",
+    "option4": "Spokes",
+    "answer": "4"
+}, {
+    "question": "What is the name of the part of a bicycle frame that the fork steerer passes through?",
+    "option1": "Top Tube",
+    "option2": "Head Tube",
+    "option3": "Down Tube",
+    "option4": "Bottom Bracket",
+    "answer": "2"
+}, {
+    "question": "To which part of the bicycle is the tyre mounted?",
+    "option1": "Hub",
+    "option2": "Spoke",
+    "option3": "Rim",
+    "option4": "Seat Stay",
+    "answer": "3"
+}, {
+    "question": "Where is the RED BULL RAMPAGE held at?",
+    "option1": "ANZ Stadium",
+    "option2": "Grand Canyon",
+    "option3": "Tokyo, Japan",
+    "option4": "Zion National Park in Virgin",
+    "answer": "4"
+},
+]
