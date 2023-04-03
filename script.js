ee6561d7857af6b613d2cbcbfebd8b8112ed92e8
Max Lockwood <mlockwood@hotmail.co.uk>
2023-04-03 06:53:22 +0100

    Refactor: js script

--- script.js
+++ script.js
@@ -13,40 +13,40 @@ var nextButton = document.getElementById("nextButton");
 var resultCont = document.getElementById("result");
 
 //Load question
-function loadQuestion (questionIndex) {
-    var q = questions[questionIndex];
-    questionEl.textContent = (questionIndex + 1) + ". " + q.question;
-    opt1.textContent = q.option1;
-    opt2.textContent = q.option2;
-    opt3.textContent = q.option3;
-    opt4.textContent = q.option4;
-};
+function loadQuestion(questionIndex) {
+  var q = questions[questionIndex];
+  questionEl.textContent = questionIndex + 1 + ". " + q.question;
+  opt1.textContent = q.option1;
+  opt2.textContent = q.option2;
+  opt3.textContent = q.option3;
+  opt4.textContent = q.option4;
+}
 
-function loadNextQuestion () {
-    //If no radio option is selected, return an alert message
-    var selectedOption = document.querySelector("input[type=radio]:checked");
-    if(!selectedOption){
-        alert("Please select your answer!");
-        return;
-    }
-    //Comparing the answer value, if true add 10 to the score
-    var answer = selectedOption.value;
-    if(questions[currentQuestion].answer == answer) {
-        score +=10;
-    }
-    //Uncheck the current option, increment currentQuestion and load next question
-    selectedOption.checked = false;
-    currentQuestion++;
-    if(currentQuestion == totalQuestions - 1){
-        nextButton.textContent = "Finish";
-    }
-    //Hide the container to reveal the score after final question
-    if(currentQuestion == totalQuestions){
-        container.style.display = "none";
-        resultCont.style.display = "";
-        resultCont.textContent = `Your Score: ${score}/100`;
-        return;
-    }
-    loadQuestion(currentQuestion);
+function loadNextQuestion() {
+  //If no radio option is selected, return an alert message
+  var selectedOption = document.querySelector("input[type=radio]:checked");
+  if (!selectedOption) {
+    alert("Please select your answer!");
+    return;
+  }
+  //Comparing the answer value, if true add 10 to the score
+  var answer = selectedOption.value;
+  if (questions[currentQuestion].answer == answer) {
+    score += 10;
+  }
+  //Uncheck the current option, increment currentQuestion and load next question
+  selectedOption.checked = false;
+  currentQuestion++;
+  if (currentQuestion == totalQuestions - 1) {
+    nextButton.textContent = "Finish";
+  }
+  //Hide the container to reveal the score after final question
+  if (currentQuestion == totalQuestions) {
+    container.style.display = "none";
+    resultCont.style.display = "";
+    resultCont.textContent = `Your Score: ${score}/100`;
+    return;
+  }
+  loadQuestion(currentQuestion);
 }
 loadQuestion(currentQuestion);
